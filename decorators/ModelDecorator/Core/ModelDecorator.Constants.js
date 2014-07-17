671e46f85e58ecfc1fc807e8bf69f92da85bd228
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2014-07-17 15:33:50 -0500

    Add default ModelDecorator from webgme

---
+++ decorators/ModelDecorator/Core/ModelDecorator.Constants.js
@@ -0,0 +1,17 @@
+"use strict";
+/*
+ * STRING CONSTANT DEFINITIONS USED IN DECORATOR
+ */
+
+define([], function () {
+
+    //return string constants
+    return {
+        /*
+         * CLASS DEFINITIONS
+         */
+        POINTER_CLASS : 'ptr',
+        POINTER_CLASS_NON_SET : 'ptr-nonset',
+        PROGRESS_BAR_CLASS : 'progress-bar'
+    };
+});
