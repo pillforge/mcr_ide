0ddeae4de64e4655c3cf9b8238a074defe45880f
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2014-08-07 14:27:20 -0500

    Use local CodeMirror v4, add anyword autocomplete

--- decorators/ModelDecorator/DiagramDesigner/SourceDetailsDialog.js
+++ decorators/ModelDecorator/DiagramDesigner/SourceDetailsDialog.js
@@ -1,4 +1,14 @@
-define(['text!./SourceDetailsDialog.html', 'codemirror'],
+define([
+  'text!./SourceDetailsDialog.html',
+  // '../../lib/codemirror.min',
+  '../../lib/CodeMirror/lib/codemirror',
+  'css!../../lib/CodeMirror/lib/codemirror',
+  '../../lib/CodeMirror/mode/clike/clike',
+  '../../lib/CodeMirror/addon/hint/show-hint',
+  'css!../../lib/CodeMirror/addon/hint/show-hint',
+  'css!../../css/show-hint-webgme',
+  '../../lib/CodeMirror/addon/hint/anyword-hint'
+  ],
   function(sourceDetailsDialogTemplate, CodeMirror) {
 
     "use strict";
@@ -27,10 +37,27 @@ define(['text!./SourceDetailsDialog.html', 'codemirror'],
       self._el = self._dialog.find('.modal-body').first();
       self._pScript = self._el.find('#pScript').first();
       self._scriptEditor = self._pScript.find('div.controls').first();
+
+      CodeMirror.commands.autocomplete = function(cm) {
+        cm.showHint({hint: CodeMirror.hint.anyword});
+      };
+
       self._codeMirror = CodeMirror(self._scriptEditor[0], {
         value: val,
+        lineNumbers: true,
+        extraKeys: {"Ctrl-Space": "autocomplete"},
         mode: "text/x-nesc"
       });
+
+      /*
+      CodeMirror.commands.autocomplete = function(cm) {
+        cm.showHint({hint: CodeMirror.hint.anyword});
+      }
+      var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+        lineNumbers: true,
+        extraKeys: {"Ctrl-Space": "autocomplete"}
+      });
+      */
 
       self._header = self._dialog.find('h3').first();
       self._header.html('Implementation of ' + name);
