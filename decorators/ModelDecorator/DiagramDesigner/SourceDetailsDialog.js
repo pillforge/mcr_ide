bde77356544452c8ee6be31865d8588737e18bf0
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2014-08-11 17:48:36 -0500

    Insert registerHelper in SourceDetailsDialog

--- decorators/ModelDecorator/DiagramDesigner/SourceDetailsDialog.js
+++ decorators/ModelDecorator/DiagramDesigner/SourceDetailsDialog.js
@@ -6,8 +6,8 @@ define([
   '../../lib/CodeMirror/mode/clike/clike',
   '../../lib/CodeMirror/addon/hint/show-hint',
   'css!../../lib/CodeMirror/addon/hint/show-hint',
-  'css!../../css/show-hint-webgme',
-  '../../lib/CodeMirror/addon/hint/anyword-hint'
+  'css!../../css/show-hint-webgme'
+  // '../../lib/CodeMirror/addon/hint/anyword-hint'
   ],
   function(sourceDetailsDialogTemplate, CodeMirror) {
 
@@ -38,10 +38,9 @@ define([
       self._pScript = self._el.find('#pScript').first();
       self._scriptEditor = self._pScript.find('div.controls').first();
 
-      // console.dir(autocompleteData);
-
+      self._registerHelper(autocompleteData);
       CodeMirror.commands.autocomplete = function(cm) {
-        cm.showHint({hint: CodeMirror.hint.anyword});
+        cm.showHint({hint: CodeMirror.hint.nesc});
       };
 
       self._codeMirror = CodeMirror(self._scriptEditor[0], {
@@ -62,6 +61,36 @@ define([
           if (saveCallback) {
             saveCallback.call(self, val);
           }
+      });
+    };
+
+    SourceDetailsDialog.prototype._registerHelper = function (autocompleteData) {
+      CodeMirror.registerHelper("hint", "nesc", function(editor, options) {
+        var WORD = /[\w$]+/, RANGE = 500;
+        var word = options && options.word || WORD;
+        var range = options && options.range || RANGE;
+        var cur = editor.getCursor(), curLine = editor.getLine(cur.line);
+        var start = cur.ch, end = start;
+        while (end < curLine.length && word.test(curLine.charAt(end))) ++end;
+        while (start && word.test(curLine.charAt(start - 1))) --start;
+        var curWord = start != end && curLine.slice(start, end);
+
+        var list = [], seen = {};
+        var re = new RegExp(word.source, "g");
+        for (var dir = -1; dir <= 1; dir += 2) {
+          var line = cur.line, endLine = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
+          for (; line != endLine; line += dir) {
+            var text = editor.getLine(line), m;
+            while (m = re.exec(text)) {
+              if (line == cur.line && m[0] === curWord) continue;
+              if ((!curWord || m[0].lastIndexOf(curWord, 0) == 0) && !Object.prototype.hasOwnProperty.call(seen, m[0])) {
+                seen[m[0]] = true;
+                list.push(m[0]);
+              }
+            }
+          }
+        }
+        return {list: list, from: CodeMirror.Pos(cur.line, start), to: CodeMirror.Pos(cur.line, end)};
       });
     };
 
