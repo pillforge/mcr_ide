22c3402e1703a7e7f00d46401ac47a0bf177a471
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2015-03-01 19:20:16 -0600

    Gather unit data

--- plugins/RobotCompiler/RobotCompilerWorker.js
+++ plugins/RobotCompiler/RobotCompilerWorker.js
@@ -21,10 +21,21 @@ define(['logManager', '../common/Util'], function (LogManager, Util) {
       var msg_flows = populate(children, self.META.Message_Flow);
 
       for (var input_flow in input_flows) {
+        var input_flow_node = nodes[input_flow];
+        var src = self.core.getPointerPath(input_flow_node, 'src');
+        var dst = self.core.getPointerPath(input_flow_node, 'dst');
+        var src_node = nodes[src];
+        var dst_node = nodes[dst];
+        if (self.core.isTypeOf(dst_node, self.META.Port)) {
+          self.logger.warn(src + ' ' + dst);
+          var parent_of_dst = get_parent_path(dst);
+          if (!units[parent_of_dst].parameters)
+            units[parent_of_dst].parameters = {};
+          units[parent_of_dst].parameters[src] = dst;
+        }
       }
 
-
-      // self.handleUnit('/187823436/1419722536/1652068488', nodes);
+      self.handleUnit(units['/187823436/1419722536/1652068488'], nodes);
 
       callback();
 
@@ -33,16 +44,22 @@ define(['logManager', '../common/Util'], function (LogManager, Util) {
         children.forEach(function (child_id) {
           var child = nodes[child_id];
           if (self.core.isTypeOf(child, type))
-            obj[child_id] = child;
+            obj[child_id] = {
+              obj: child
+            }
         });
         return obj;
+      }
+
+      function get_parent_path (path) {
+        return path.substr(0, path.lastIndexOf('/'));
       }
 
     });
   };
 
-  RobotCompilerWorker.prototype.handleUnit = function(unit_id, nodes) {
-    
+  RobotCompilerWorker.prototype.handleUnit = function(unit, nodes) {
+    console.log(unit);
   };
 
   return RobotCompilerWorker;
