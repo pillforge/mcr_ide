0541cc6c487419a9ee07d2b73269a3fb46dda1ae
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2015-04-27 13:59:27 -0500

    Update the nodes export script

--- test/meta/export_nodes_from_meta.js
+++ test/meta/export_nodes_from_meta.js
@@ -1,6 +1,12 @@
-var meta_json = require('../../seeds/MetaDev');
-var fs = require('fs');
+var arg = process.argv[2];
+if (!arg) {
+  console.log('provide a file');
+  process.exit(1);
+}
 
+var fs = require('fs');
+var path = require('path');
+var meta_json = require(path.join(process.cwd(), arg));
 var nodes = meta_json.nodes;
 
 var exp_nodes = {};
@@ -10,4 +16,7 @@ for (var key in nodes) {
   exp_nodes[name] = {};
 }
 
-fs.writeFileSync('nodes_generated.json', JSON.stringify(exp_nodes, null, '  '));
+fs.writeFileSync(
+  path.join(__dirname, 'gen-' + arg.split('/').slice(-1)),
+  JSON.stringify(exp_nodes, null, '  ')
+);
