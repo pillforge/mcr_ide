02b34af037abab2cabe9fad75016d3fd7fb91393
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2015-06-04 16:41:51 -0500

    Use custom WebGME
    Remove showing the children of interface-definition visually instead
    create the children of interface-definition when it is referenced with
    'interface' pointer.

--- decorators/TosDecorator/DiagramDesigner/TosDecorator.DiagramDesignerWidget.js
+++ decorators/TosDecorator/DiagramDesigner/TosDecorator.DiagramDesignerWidget.js
@@ -13,27 +13,6 @@ function (ModelDecoratorDiagramDesignerWidget) {
   };
   _.extend(TosDecoratorDiagramDesignerWidget.prototype, ModelDecoratorDiagramDesignerWidget.prototype);
 
-  /**
-   * Returns the children of the object and the referred interface's children
-   * @override
-   */
-  TosDecoratorDiagramDesignerWidget.prototype.getPortIDs = function () {
-    var children_ids = ModelDecoratorDiagramDesignerWidget.prototype.getPortIDs.call(this);
-    
-    var client = this._control._client;
-    var node_obj = client.getNode(this._metaInfo.GME_ID);
-
-    if (_.contains(node_obj.getPointerNames(), 'interface') ) {
-      var pointer_id = node_obj.getPointer('interface').to;
-      var pointer_obj = client.getNode(pointer_id);
-      if (pointer_obj) {
-        children_ids = children_ids.concat(pointer_obj.getChildrenIds());
-      }
-    }
-
-    return children_ids;
-  };
-
   return TosDecoratorDiagramDesignerWidget;
 
 });
