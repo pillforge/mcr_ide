b1a2f56a3ca533573f61273403c3608891c5032d
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2015-07-02 14:20:09 -0500

    Load wiring end nodes synchronously
    Fails to access nodes' attributes due to a WebGME bug

--- plugins/TinyOSWiringPopulater/TinyOSWiringPopulater.js
+++ plugins/TinyOSWiringPopulater/TinyOSWiringPopulater.js
@@ -1,5 +1,5 @@
-define(['plugin/PluginBase', 'plugin/PluginConfig'],
-function (PluginBase, PluginConfig) {
+define(['plugin/PluginBase', 'plugin/PluginConfig', '../utils/Constants'],
+function (PluginBase, PluginConfig, Constants) {
 
   'use strict';
 
@@ -92,7 +92,58 @@ function (PluginBase, PluginConfig) {
   };
 
   TinyOSWiringPopulater.prototype.createConfigurationWirings = function (config_wgme_paths, next) {
-    next();
+    var self = this;
+    var core = self.core;
+    var async = require('async');
+    var instances = {};
+
+    async.forEachOf(config_wgme_paths, function (value, key, callback) {
+      var node = self._nodes[key];
+      var config_dump = core.getRegistry(node, 'nesc-dump');
+      wire_configuration(key, node, config_dump.wiring);
+      callback();
+    }, function (err) {
+      next();
+    });
+    
+    function wire_configuration (config_name, node, wirings) {
+      for (var i = wirings.length - 1; i >= 0; i--) {
+        var wire = wirings[i];
+        var fi_node = get_interface(config_name, node, wire.from);
+        var ti_node = get_interface(config_name, node, wire.to);
+        if (fi_node && ti_node)
+          self.logger.info(config_name, core.getAttribute(fi_node, 'name'), core.getAttribute(ti_node, 'name') );
+      }
+    }
+
+    function get_interface (config_name, node, end) {
+      
+      if (config_name === end.name)
+        return self._nodes[self.joinPath(end.name, end.interface)];
+      
+      if ( !(end.name in instances) ) {
+
+        var instance_node = core.createNode({
+          base: self._nodes[end.name],
+          parent: node
+        });
+        instances[end.name] = instance_node;
+
+        var c_ids = core.getChildrenRelids(instance_node);
+        for (var i = c_ids.length - 1; i >= 0; i--) {
+          var child = self.core.getChild(instance_node, c_ids[i]);
+          var child_name = self.core.getAttribute(child, 'name');
+          instances[self.joinPath(end.name, child_name)] = child;
+        }
+      }
+
+      return instances[self.joinPath(end.name, end.interface)];
+    }
+
+  };
+
+  TinyOSWiringPopulater.prototype.joinPath = function (a, b) {
+    return [a, b].join(Constants.DELIMITER);
   };
 
   TinyOSWiringPopulater.prototype.createModuleCalls = function (module_wgme_paths, next) {
