0b2cb9481c98aee14695e0882a1c5da1fe6e91e0
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2015-12-30 17:47:14 -0500

    Save the source of a module to a temp folder

--- src/plugins/utils/ModuleUtil.js
+++ src/plugins/utils/ModuleUtil.js
@@ -1,11 +1,32 @@
-define([], function () {
+define(['Q', 'project_root/plugins/utils/NescUtils', 'path', 'fs-extra'],
+function (Q, nesc_utils, path, fs) {
 
 'use strict';
 
-return {
-  generateModule: function () {
-
+var obj = {
+  generateModule: function (core, module_node) {
+    return Q.fcall(function () {
+      return obj._saveSource(core, module_node);
+    })
+    .then(function (file_path) {
+      return Q.nfcall(nesc_utils.getAppJson, file_path);
+    })
+    .then(function (app_json) {
+    });
+  },
+  _saveSource: function (core, module_node) {
+    return Q.fcall(function () {
+       var random_folder_name = Math.random().toString(36).substring(7);
+       var tmp_path = path.join('/tmp', random_folder_name);
+       var file_path = path.join(tmp_path, core.getAttribute(module_node, 'name'));
+       file_path += '.nc';
+       var src = core.getAttribute(module_node, 'source');
+       fs.outputFileSync(file_path, src);
+       return file_path;
+    });
   }
 };
+
+return obj;
 
 });
--- test/plugins/utils/ModuleUtil.spec.js
+++ test/plugins/utils/ModuleUtil.spec.js
@@ -4,6 +4,7 @@ var testFixture = require('../../globals');
 describe('ModuleUtil', function () {
 
   var module_util = testFixture.requirejs('project_src/plugins/utils/ModuleUtil');
+  var fs = require('fs');
   var gmeConfig = testFixture.getGmeConfig();
   var gmeAuth;
   var storage;
@@ -13,6 +14,7 @@ describe('ModuleUtil', function () {
   var project;
   var Q = testFixture.Q;
   var expect = testFixture.expect;
+  var blinkC_node;
 
   before(function (done) {
     testFixture.clearDBAndGetGMEAuth(gmeConfig, null)
@@ -46,10 +48,11 @@ describe('ModuleUtil', function () {
 
   it('should have defined properties', function (done) {
     module_util.should.have.property('generateModule');
+    module_util.should.have.property('_saveSource');
     done();
   });
 
-  it('the imported project should have apps, Blink and BlinkC objects', function (done) {
+  it('should import a project with apps, Blink and BlinkC objects', function (done) {
     Q.nfcall(context.core.loadChildren, context.rootNode)
       .then(function (children) {
         expect(children).not.to.equal(null);
@@ -71,10 +74,34 @@ describe('ModuleUtil', function () {
         expect(children).not.to.equal(null);
         var found = false;
         for (var i = children.length - 1; i >= 0; i--) {
-          if ('BlinkC' == context.core.getAttribute(children[i], 'name'))
+          if ('BlinkC' == context.core.getAttribute(children[i], 'name')) {
             found = true;
+            blinkC_node = children[i];
+          }
         }
         expect(found).to.equal(true);
+      })
+      .nodeify(done);
+  });
+
+  it('should save source to a temp folder', function (done) {
+    module_util._saveSource(context.core, blinkC_node)
+      .then(function (tmp_path) {
+        expect(tmp_path).to.contain('tmp');
+        expect(fs.existsSync(tmp_path)).to.equal(true);
+      })
+      .nodeify(done);
+  });
+
+  xit('should generate uses and provides interfaces', function (done) {
+    module_util.generateModule(context.core, blinkC_node)
+      .then(function () {
+        return Q.nfcall(context.core.loadChildren, blinkC_node);
+      })
+      .then(function (children) {
+        for (var i = children.length - 1; i >= 0; i--) {
+          console.log(context.core.getAttribute(children[i], 'name'));
+        }
       })
       .nodeify(done);
   });
