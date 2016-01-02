80caf18e10e59bd4d1807e87b00458a7f2431ed8
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2016-01-01 20:00:04 -0500

    Convert ModuleUtil from an object to a class

--- src/plugins/utils/ModuleUtil.js
+++ src/plugins/utils/ModuleUtil.js
@@ -3,46 +3,39 @@ function (Q, nesc_utils, wgme_utils, path, fs) {
 
 'use strict';
 
-var module_name;
-
-var obj = {
-  generateModule: function (context, module_node) {
-    return Q.fcall(function () {
-      return obj._saveSource(context.core, module_node);
-    })
-    .then(function (file_path) {
-      return Q.nfcall(nesc_utils.getAppJson, file_path);
-    })
-    .then(function (app_json) {
-      var comp_json = app_json.components[module_name];
-    });
-  },
-  _saveSource: function (core, module_node) {
-    return Q.fcall(function () {
-       var random_folder_name = Math.random().toString(36).substring(7);
-       var tmp_path = path.join('/tmp', random_folder_name);
-       module_name = core.getAttribute(module_node, 'name');
-       var file_path = path.join(tmp_path, module_name);
-       file_path += '.nc';
-       var src = core.getAttribute(module_node, 'source');
-       fs.outputFileSync(file_path, src);
-       return file_path;
-    });
-  },
-  _getMetaNodes: function (context) {
-    return Q.fcall(function () {
-      if (!context.META) {
-        context.META = {};
-        var metanodes = context.core.getAllMetaNodes(context.rootNode);
-        Object.keys(metanodes).forEach(function (key) {
-          var name = context.core.getAttribute(metanodes[key], 'name');
-          context.META[name] = metanodes[key];
-        });
-      }
-    });
-  }
+var ModuleUtil = function (context, module_node) {
+  this._context = context;
+  this._core = context.core;
+  this._module_node = module_node;
+  this._module_name = context.core.getAttribute(module_node, 'name');
 };
 
-return obj;
+ModuleUtil.prototype.generateModule = function() {
+};
+
+ModuleUtil.prototype._saveSource = function() {
+  return Q.fcall(function () {
+    var random_folder_name = Math.random().toString(36).substring(7);
+    var tmp_path = path.join('/tmp', random_folder_name);
+    var file_path = path.join(tmp_path, this._module_name + '.nc');
+    fs.outputFileSync(file_path, this._core.getAttribute(this._module_node, 'source'));
+    return file_path;
+  }.bind(this));
+};
+
+ModuleUtil.prototype._getMetaNodes = function() {
+  return Q.fcall(function () {
+    if (!this._context.META) {
+      this._context.META = {};
+      var metanodes = this._core.getAllMetaNodes(this._context.rootNode);
+      Object.keys(metanodes).forEach(function (key) {
+        var name = this._core.getAttribute(metanodes[key], 'name');
+        this._context.META[name] = metanodes[key];
+      }.bind(this));
+    }
+  }.bind(this));
+};
+
+return ModuleUtil;
 
 });
--- test/plugins/utils/ModuleUtil.spec.js
+++ test/plugins/utils/ModuleUtil.spec.js
@@ -3,18 +3,23 @@ var testFixture = require('../../globals');
 
 describe('ModuleUtil', function () {
 
-  var module_util = testFixture.requirejs('project_src/plugins/utils/ModuleUtil');
-  var fs = require('fs');
   var gmeConfig = testFixture.getGmeConfig();
   var gmeAuth;
+  var logger = testFixture.logger.fork('plugins/utils/ModuleUtil');
   var storage;
   var projectName = 'ModuleUtilTest';
-  var logger = testFixture.logger.fork('plugins/utils/ModuleUtil');
-  var context;
   var project;
+  var context;
+
   var Q = testFixture.Q;
+
   var expect = testFixture.expect;
   var blinkC_node;
+
+  var ModuleUtil = testFixture.requirejs('project_src/plugins/utils/ModuleUtil');
+  var module_util;
+
+  var fs = require('fs');
 
   before(function (done) {
     testFixture.clearDBAndGetGMEAuth(gmeConfig, null)
@@ -46,13 +51,6 @@ describe('ModuleUtil', function () {
     .nodeify(done);
   });
 
-  it('should have defined properties', function (done) {
-    module_util.should.have.property('generateModule');
-    module_util.should.have.property('_saveSource');
-    module_util.should.have.property('_getMetaNodes');
-    done();
-  });
-
   it('should import a project with apps, Blink and BlinkC objects', function (done) {
     Q.nfcall(context.core.loadChildren, context.rootNode)
       .then(function (children) {
@@ -75,9 +73,10 @@ describe('ModuleUtil', function () {
         expect(children).not.to.equal(null);
         var found = false;
         for (var i = children.length - 1; i >= 0; i--) {
-          if ('BlinkC' == context.core.getAttribute(children[i], 'name')) {
+          if (!found && 'BlinkC' == context.core.getAttribute(children[i], 'name')) {
             found = true;
             blinkC_node = children[i];
+            module_util = new ModuleUtil(context, blinkC_node);
           }
         }
         expect(found).to.equal(true);
@@ -85,35 +84,61 @@ describe('ModuleUtil', function () {
       .nodeify(done);
   });
 
-  it('should save source to a temp folder', function (done) {
-    module_util._saveSource(context.core, blinkC_node)
-      .then(function (tmp_path) {
-        expect(tmp_path).to.contain('tmp');
-        expect(fs.existsSync(tmp_path)).to.equal(true);
-      })
-      .nodeify(done);
+  it('should have defined properties', function (done) {
+    expect(module_util).to.be.an('object');
+    module_util.should.have.property('generateModule');
+    module_util.should.have.property('_saveSource');
+    module_util.should.have.property('_getMetaNodes');
+    done();
   });
 
-  it('should get meta nodes', function (done) {
-    module_util._getMetaNodes(context)
-      .then(function () {
-        expect(context.META).to.be.an('object');
-      })
-      .nodeify(done);
+  describe('#generateModule', function () {
+    var nesc_utils = testFixture.requirejs('project_root/plugins/utils/NescUtils');
+    var blinkc_json;
+    before(function (done) {
+      module_util._saveSource(context.core, blinkC_node)
+        .then(function (file_path) {
+          return Q.nfcall(nesc_utils.getAppJson, file_path);
+        })
+        .then(function (app_json) {
+          blinkc_json = app_json;
+        })
+        .nodeify(done);
+    });
+
+    xit('should generate uses and provides interfaces', function (done) {
+      module_util.generateModule(context, blinkC_node)
+        .then(function () {
+          return Q.nfcall(context.core.loadChildren, blinkC_node);
+        })
+        .then(function (children) {
+          for (var i = children.length - 1; i >= 0; i--) {
+            console.log(context.core.getAttribute(children[i], 'name'));
+          }
+        })
+        .nodeify(done);
+    });
   });
 
-  xit('should generate uses and provides interfaces', function (done) {
-    module_util.generateModule(context, blinkC_node)
-      .then(function () {
-        return Q.nfcall(context.core.loadChildren, blinkC_node);
-      })
-      .then(function (children) {
-        for (var i = children.length - 1; i >= 0; i--) {
-          console.log(context.core.getAttribute(children[i], 'name'));
-        }
-      })
-      .nodeify(done);
+  describe('#_saveSource', function () {
+    it('should save source to a temp folder', function (done) {
+      module_util._saveSource()
+        .then(function (tmp_path) {
+          expect(tmp_path).to.contain('tmp');
+          expect(fs.existsSync(tmp_path)).to.equal(true);
+        })
+        .nodeify(done);
+    });
   });
 
+  describe('#_getMetaNodes', function () {
+    it('should get meta nodes', function (done) {
+      module_util._getMetaNodes(context)
+        .then(function () {
+          expect(context.META).to.be.an('object');
+        })
+        .nodeify(done);
+    });
+  });
 
 });
