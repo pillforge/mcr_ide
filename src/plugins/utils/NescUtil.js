acaa1d6ce15066ca0bd18f1de678a45d89cf6aed
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2016-01-05 15:31:46 -0600

    Add schema for nesc dump

--- src/plugins/utils/NescUtil.js
+++ src/plugins/utils/NescUtil.js
@@ -1,4 +1,5 @@
-define(['q', 'path', 'fs-extra', 'project_root/plugins/common/ParseDump'], function (Q, path, fs, pd) {
+define(['q', 'path', 'fs-extra', 'project_root/plugins/common/ParseDump'],
+function (Q, path, fs, pd) {
 
 'use strict';
 
@@ -10,16 +11,17 @@ return {
       var cmd = [
         'ncc',
         '-target=' + target,
-        '-fnesc-dump=interfacedefs',
         '-fnesc-dump=components',
+        '-fnesc-dump=interfacedefs',
         '-fnesc-dump=interfaces',
         '-fsyntax-only',
         '-get-calls=' + get_calls_file,
         file_path
-      ].join(' ');
-      var app_json = pd.parse(execSync(cmd, {
+      ].map(function (e) { return "'" + e + "'"; }).join(' ');
+      var xml = execSync(cmd, {
         encoding: 'utf8'
-      }));
+      });
+      var app_json = pd.parse(xml);
       app_json.calls = fs.readJsonSync(get_calls_file);
       return app_json;
     });
--- test/plugins/utils/NescUtil.spec.js
+++ test/plugins/utils/NescUtil.spec.js
@@ -1,11 +1,11 @@
-'use strict';
-var testFixture = require('../../globals');
-
 describe('NescUtil', function () {
-
+  'use strict';
+  var testFixture = require('../../globals');
   var nesc_util = testFixture.requirejs('project_src/plugins/utils/NescUtil');
   var expect = testFixture.expect;
   var path = require('path');
+  var fs = require('fs-extra');
+  require('chai').use(require('chai3-json-schema'));
 
   it('should have defined properties', function (done) {
     expect(nesc_util).to.be.an('object');
@@ -22,16 +22,20 @@ describe('NescUtil', function () {
         })
         .nodeify(done);
     });
+
+    it('should comply with the schema', function (done) {
+      var schema = fs.readJsonSync(path.join(__dirname, 'NescUtil/AppSchema.json'));
+      app_json.should.be.jsonSchema(schema);
+      done();
+    });
+
     it('should have BlinkC component', function (done) {
-      expect(app_json).to.be.an('object');
-      app_json.should.have.property('components');
       app_json.components.should.have.property('BlinkC');
       done();
     });
+
     it('should have BlinkC interfaces', function (done) {
-      var blinkc_json = app_json.components.BlinkC;
-      blinkc_json.should.have.property('interface_types');
-      var blinkc_interfaces = blinkc_json.interface_types;
+      var blinkc_interfaces = app_json.components.BlinkC.interface_types;
       blinkc_interfaces.should.have.length(5);
       done();
     });
